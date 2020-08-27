1e80ef032a522a0ec0a9d91e182c1f21e2014ace
maria alvarez <mar.alvareg@gmail.com>
2020-08-27 18:29:45 -0400

    notas

--- calculo_notas.rb
+++ calculo_notas.rb
@@ -2,14 +2,8 @@ data = File.open('notas.data').readlines
 data.map!{|line| line.split(",")}
 
 def nota_mas_alta(student)
- name = student.delete_at(0)
- grades = student.map{|e| e.to_i}
- max_grade = grades.max
- return max_grade
+ [student[0], student.map{|x| x.to_i}.max]
 end
 
-data = open('notas.data').readlines.map(&:chomp)
-data_student = data.map{|v| v.split(',')}
-
-puts nota_mas_alta(data_student[0])
-puts nota_mas_alta(data_student[1])
+puts nota_mas_alta(data[0])
+puts nota_mas_alta(data[1])
