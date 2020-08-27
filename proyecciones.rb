be2137ff66155edf830ea85b62d9af34334c4fed
maria alvarez <mar.alvareg@gmail.com>
2020-08-27 18:19:04 -0400

    proyeccion

--- proyecciones.rb
+++ proyecciones.rb
@@ -1,17 +1,16 @@
 data = File.open('ventas_base.db').read.split(",")
-arr= data.map!{|x| x.to_i}
+arr= data.map!{|x| x.to_f}
 
 def proyeccion (arr, aumento, desde, hasta)
     filtered_arr = arr[desde..hasta]
-    suma_total = arr.sum - filtered_arr.sum
-    a = filtered_arr.sum.to_f * aumento
-    a.to_f + suma_total
+    sum =filtered_arr.map{|x| x*aumento}.sum
 end
 
-primero = proyeccion(arr, 1.1, 0, 5).round(2)
-segundo = proyeccion(arr, 1.2, 6, 11).round(2)
+primero = proyeccion(arr, 1.1, 0, 5)
+segundo = proyeccion(arr, 1.2, 6, 11)
 
 first = primero.to_s
 second = segundo.to_s
 
 File.write('resultados.data', first + ("\n") + second)
+
